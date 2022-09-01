1bc00cc38cb4fe7d42015380841eb45e6076458d
Andrei-Florin Ciobanu <andrei1805florin@gmail.com>
2022-09-01 14:32:43 +0300

    Added Utils scripts

---
+++ Assets/Scripts/Utils/UnitySingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class UnitySingleton<T> : MonoBehaviour where T : Component{
+    private static T s_instance;
+    public static T Instance {
+        get {
+            if (s_instance == null) {
+                s_instance = FindObjectOfType<T> ();
+            }
+            return s_instance;
+        }
+    }
+ 
+    public virtual void Awake () {
+        if (s_instance == null) {
+            s_instance = this as T;
+        } else {
+            Destroy (gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/UserSettingsManager.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * Created Date: 08/23/2022
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2022 Andrei-Florin Ciobanu. All rights reserved. 
+ */
+
+using UnityEngine;
+
+public class UserSettingsManager : UnitySingleton<UserSettingsManager>{
+    private const string CURRENT_LEVEL_INDEX = "c9e9a848920877e76685b2e4e76de38d";
+    public int CurrentLevelIndex{ get; set; }
+    
+    #region Save Settings
+    
+    public void SaveSettings() {
+        PlayerPrefs.SetInt(CURRENT_LEVEL_INDEX, this.CurrentLevelIndex);
+    }
+    
+    #endregion
+    
+    #region Load Settings
+
+    public void LoadSettings() {
+        this.CurrentLevelIndex = PlayerPrefs.GetInt(CURRENT_LEVEL_INDEX, 0);
+    }
+    
+    #endregion
+}
